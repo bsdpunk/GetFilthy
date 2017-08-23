0508806d2da251421d2f50861b95ba78efed89e7
bsdpunk <bsdpunk@gmail.com>
2017-08-23 13:44:18 -0500

    port/addy

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "webSphere %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "webSphere %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("http://Sphere-bsdpunk771718.codeanyapp.com:8080", nil)
 }
